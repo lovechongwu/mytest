666cadcba11013f2474be7214206f985f80e7284
zhumeng <2856814420@qq.com>
2017-08-30 16:32:34 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import jquery from 'jquery'
+import './assets/bootstrap/bootstrap.min.js'
+import './assets/bootstrap/bootstrap.min.css'
+import './assets/bootstrap/style.css'
+import './assets/js/flexible.js'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
